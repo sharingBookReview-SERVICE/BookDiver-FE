cc135024aa970ca8bf3df46dd3c8169c0baa0222
노예찬 <noy3928@naver.com>
2021-07-26 00:57:15 +0900

    기본세팅

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './shared/App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import store from "./redux/ConfigStore";
+import "bootstrap/dist/css/bootstrap.css"
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/redux/configStore.js
@@ -0,0 +1,33 @@
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import { createBrowserHistory } from "history";
+import { connectRouter } from "connected-react-router";
+import thunk from "redux-thunk";
+
+
+const history = createBrowserHistory();
+const rootReducer = combineReducers({
+  
+  router: connectRouter(history),
+});
+
+const middlewares = [thunk.withExtraArgument({ history })];
+
+const env = process.env.NODE_ENV;
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+
+const store = createStore(rootReducer, enhancer);
+
+export { history };
+export default store;
