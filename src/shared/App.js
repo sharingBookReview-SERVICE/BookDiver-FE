8da690cacbacad6381d6753dbebc15bc2b5ee237
leehyeonj <billaa@naver.com>
2021-07-26 18:48:45 +0900

    [추가] template추가, App.js Route설정

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,24 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import { ConnectedRouter } from 'connected-react-router';
+import { Route } from "react-router-dom";
+import {history} from "../redux/configStore";
 
 function App() {
   return (
+    <ConnectedRouter history={history}>
     <div className="App">
       <Container>
-
+      {/* <Route path="/" exact component={} /> */}
       </Container>
     </div>
+    </ConnectedRouter>
   );
 }
 
 const Container = styled.div`
   width: 360px;
   height: 720px;
+  background: #fff;
   overflow-y: scroll;
   overflow-x: hidden;
-  &::-webkit-scrollbar {
-    display: none;
-  }
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+    &::-webkit-scrollbar {
+        display: none; /* Chrome, Safari, Opera*/
   background-color: #fff;
   border-radius: 5px;
   display: flex;
---
+++ src/shared/template.js
@@ -0,0 +1,40 @@
+// //import 부분
+// import React, { useState } from "react";
+
+
+
+// const Comment = (props) =>{
+//   //dispatch와 변수들
+//     const dispatch = useDispatch();
+//     const {replyId, replyContent, replyWriter}= props;
+
+// //useEffect
+// React.useEffect(()=>{
+// },[]);
+
+
+// //함수
+// const deleteReply = ()=>{
+   
+//     }
+
+// //뷰
+
+//     return(
+//         <React.Fragment>
+         
+//         </React.Fragment>
+//     )
+// }
+
+// //defaultProps
+// Comment.defaultProps={
+// }
+
+
+// //styled components
+// const PasswordPop = styled.div`
+    
+// `;
+
+// export default Comment;
