7ce1190d4780f3252a224e37236dcc3746002a6b
노예찬 <noy3928@naver.com>
2021-07-27 15:20:18 +0900

    [추가중] 작업을 하던중 feature로 들어가서 작업한다는 것을 깜빡함

---
+++ src/components/Comment.js
@@ -0,0 +1,35 @@
+//import 부분
+import React, { useState } from "react";
+import styled from "styled-components"
+
+
+const Comment = (props) =>{
+  //dispatch와 변수들
+
+    return(
+        <React.Fragment>
+         
+        </React.Fragment>
+    )
+}
+
+const CommentBox = styled.div`
+width: 336px;
+  height: 136px;
+  flex-grow: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 8px;
+  padding: 24px;
+`
+
+const UserName = styled.p`
+
+`
+
+const CommentContent = styled.p`
+`
+
+export default Comment;
