8f1942300e999df92cb2f67f3311dc3b2c69b7c3
yechan <noy3928@naver.com>
2021-07-28 11:36:32 +0900

    [추가] 댓글관련 서버통신 함수 작성완료
    작성은 완료했지만, 아직 테스트를 해보지 않았기 때문에 미완이라고 볼 수 있습니다

--- src/redux/modules/comment.js
+++ src/redux/modules/comment.js
@@ -1,18 +1,19 @@
 import { createAction, handleActions } from "redux-actions";
 import { produce } from "immer";
 import instance from "../../shared/Request";
+import {history} from "../configStore"
 
 
 //actions
 const GET_COMMENT = "comment/GET_COMMENT";
 const ADD_COMMENT = "comment/ADD_COMMENT";
-const UPDATE_COMMENT = "comment/UPDATE_COMMENT";
+const EDIT_COMMENT = "comment/EDIT_COMMENT";
 const DELETE_COMMENT = "comment/DELETE_COMMENT";
 
 //actioncreator
 const getComment = createAction(GET_COMMENT, (comment) => ({ comment }));
 const addComment = createAction(ADD_COMMENT, (comment) => ({ comment }));
-const updateComment = createAction(UPDATE_COMMENT, (id, content) => ({id, content}));
+const editComment = createAction(EDIT_COMMENT, (id, content) => ({id, content}));
 const deleteComment = createAction(DELETE_COMMENT, (id) => ({id}));
 
 //initial
@@ -24,27 +25,28 @@ const initialState = {
 };
 
 
-
-//middle
-
 //comment를 서버에 add 하는 함수를 만들던중.
-// const addCommentSV = (commentInfo) => {
-//     return function(dispatch){
-//       //오늘 목표 불러오기
-//       instance.post(`/books/:${commentInfo.bookId}/reviews/:${commentInfo.reviewId}/comments`,{
-//           user:{commentInfo.userInfo},
-//           content: commentInfo.content,
-//       })
-//         .then(function (response) {
+const addCommentSV = (commentInfo) => {
+    const userInfo = commentInfo.userInfo
+    const content = commentInfo.content
 
-//             dispatch(setTodayPlan(response.data));
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//         })
+    return function(dispatch){
+      //오늘 목표 불러오기
+      instance.post(`/books/:${commentInfo.bookId}/reviews/:${commentInfo.reviewId}/comments`,{
+          user:userInfo,
+          content: content,
+      })
+        .then((res) => {
+            console.log(res);
+            dispatch(addComment(res.data));
+          
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 
-//     }
-// }
+    }
+}
 
 const updateCommentSV = (comment_info) => {
   return function (dispatch, getState, {history}){
@@ -52,14 +54,12 @@ const updateCommentSV = (comment_info) => {
     .put(`books/:${comment_info.bookId}/reviews/:${comment_info.reviewId}/comments/:${comment_info.commentId}`,
     {content : comment_info.content})
     .then((res) => {
-      if(comment_info.commentId){
-        const comment = res.data.filter((l,idx) => {
-          return l.id === parseInt(comment_info.commentId);
-        })
-      }
+      console.log(res.data)
+      dispatch(editComment())
     })
   }
 }
+
 
 const deleteCommentSV = (comment_info) => {
   return function (dispatch, getState, {history}){
@@ -75,7 +75,6 @@ const deleteCommentSV = (comment_info) => {
 
 
 
-
 //reducer
 export default handleActions(
     {
@@ -87,7 +86,7 @@ export default handleActions(
         produce(state, (draft) => {
           draft.comment_list.unshift(action.payload.comment);
         }),
-        [UPDATE_COMMENT]: (state, action) => produce(state, (draft) => {
+        [EDIT_COMMENT]: (state, action) => produce(state, (draft) => {
           let idx = draft.comment_list.findIndex((l) => l.id === action.payload.id);
           draft.comment_list[idx].content = action.payload.content;
         }),
