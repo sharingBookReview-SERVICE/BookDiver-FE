fa386001b77358cd4af31996ce833110643eb030
leehyeonj <billaa@naver.com>
2021-07-29 10:44:00 +0900

    [수정] SelectBookModal 배경색, 가운데 맞춤

--- src/shared/App.js
+++ src/shared/App.js
@@ -14,7 +14,7 @@ import ChangeName from "../pages/ChangeName";
 import MyReview from "../pages/MyReview";
 import PostWrite from "../pages/PostWrite"
 import BookDetail from "../pages/BookDetail";
-import LogoutModal from "../modals/LogoutModal";
+import SelectBookModal from "../modals/SelectBookModal";
 
 function App() {
   const is_nav = useSelector(state => state.permit.is_nav)
@@ -31,7 +31,7 @@ function App() {
           <Route path="/myprofile" exact component={MyProfile} />
           <Route path="/changename" exact component={ChangeName} />
           <Route path="/myreview" exact component={MyReview} />
-          <Route path="/modal" exact component={LogoutModal}/>
+          <Route path="/modal" exact component={SelectBookModal}/>
          </ConnectedRouter>
         {is_nav ? <Navigation/> : ""}
        </Container>
