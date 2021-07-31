11f70ef37336d8e9289d219aedd5665d740fe155
박혜민 <hyemin085@gmail.com>
2021-07-31 22:25:06 +0900

    [추가] 수정, 삭제 모달 추가

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -7,60 +7,67 @@ import BookImg from "../img/bookImg2.jpg"
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
-import { useSelector, useDispatch } from "react-redux";
-import { actionCreators as reviewActions } from "../redux/modules/review"
+import {useSelector, useDispatch} from "react-redux";
+import {actionCreators as reviewActions} from "../redux/modules/review"
+import {actionCreators as permitActions} from "../redux/modules/permit";
 import {history} from "../redux/configStore";
 
 import EditModal from "../modals/EditModal";
 
-const ReviewCard = (props) =>{
-  //dispatch와 변수들
-  const {content, hashtags, quote, created_at, bookId} = props.review;
 
-  const dispatch = useDispatch();
 
-    const is_modal = useSelector(state=> state.permit.is_modal);
-  
-  // const is_liked = useSelector(state => state.review.all_review_list[0].myLike)
+const ReviewCard = (props) => {
+    //dispatch와 변수들
+    const {content, hashtags, quote, created_at, bookId} = props.review;
+    const dispatch = useDispatch();
 
-  //서버 들어오기 전 임의로 좋아요 갯수 구하는 형식 만들어놓기
+    const is_modal = useSelector(state => state.permit.is_modal);
+
+    // const is_liked = useSelector(state => state.review.all_review_list[0].myLike)
+
+    //서버 들어오기 전 임의로 좋아요 갯수 구하는 형식 만들어놓기
 //   const reviewId = 1;
 //   const idx = reviewList.findIndex((l) => l.id === reviewId);
 //   const likesCount = reviewList[idx].likes
 
-  //좋아요 클릭 
-  const clickLikeButton = () => {
-    //props로부터 bookId와 reviewId를 받아오기
-    dispatch(reviewActions.LikeSV());
-  }
+    //좋아요 클릭
+    const clickLikeButton = () => {
+        //props로부터 bookId와 reviewId를 받아오기
+        dispatch(reviewActions.LikeSV());
+    }
 
 
-    return(
+    return (
         <React.Fragment>
+            {
+                is_modal && <EditModal/>
+            }
             <CardBox>
 
                 <CommentUserBox>
                     <UserLeftBox>
-                      <UserName>
-                          닉네임닉네임
-                      </UserName>
-                      <CreatedAt>
-                          {created_at}
-                      </CreatedAt>
+                        <UserName>
+                            닉네임닉네임
+                        </UserName>
+                        <CreatedAt>
+                            {created_at}
+                        </CreatedAt>
                     </UserLeftBox>
 
                     <UserRightBox>
-                      <BookmarkBorderIcon style={{color:"#9e9e9e", marginRight:"10px"}}/>
-                      <MoreHorizIcon style={{color:"#9e9e9e"}}/>
+                        <BookmarkBorderIcon style={{color: "#9e9e9e", marginRight: "10px"}}/>
+                        <MoreHorizIcon style={{color: "#9e9e9e"}} onClick = {() => {
+                            dispatch(permitActions.showModal(true))
+                        }}/>
                     </UserRightBox>
                 </CommentUserBox>
 
-                <Image src={BookImg} onClick={()=>{
-                  history.push('/reviewdetail')
+                <Image src={BookImg} onClick={() => {
+                    history.push('/reviewdetail')
                 }}/>
 
-                <ContentBox onClick={()=>{
-                  history.push('/reviewdetail')
+                <ContentBox onClick={() => {
+                    history.push('/reviewdetail')
                 }}>
 
                     <BookTitle>{bookId.title} | {bookId.author} </BookTitle>
@@ -84,7 +91,7 @@ const ReviewCard = (props) =>{
                     </WriteCommentBox>
                 </LikeCommentBox>
             </CardBox>
-            
+
 
         </React.Fragment>
     )
@@ -98,120 +105,120 @@ const CardBox = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
-  box-sizing:border-box;
-  margin:0px 0px 6px 0px;
-  background-color:#fff;
+  box-sizing: border-box;
+  margin: 0px 0px 6px 0px;
+  background-color: #fff;
 `
 
 const CommentUserBox = styled.div`
-display:flex;
-align-items:center;
-padding:16px 24px;
-justify-content:space-between;
-width:100%;
-box-sizing:border-box;
+  display: flex;
+  align-items: center;
+  padding: 16px 24px;
+  justify-content: space-between;
+  width: 100%;
+  box-sizing: border-box;
 `
 
 const UserLeftBox = styled.div`
-width:auto;
-height:auto;
-display:flex;
-align-items:center;
+  width: auto;
+  height: auto;
+  display: flex;
+  align-items: center;
 `
 
 const UserRightBox = styled.div`
-width:auto;
-height:auto;
-display:flex;
-align-items:center;
+  width: auto;
+  height: auto;
+  display: flex;
+  align-items: center;
 `
 
 const Image = styled.img`
-  width:auto;
-  height:auto;
-  max-width:100%;
-  max-height:100%;
+  width: auto;
+  height: auto;
+  max-width: 100%;
+  max-height: 100%;
 `
 
 const UserName = styled.p`
-font-size:14px;
-font-weight:bold;
-margin:0px 8px 0px 0px;
+  font-size: 14px;
+  font-weight: bold;
+  margin: 0px 8px 0px 0px;
 `
 
 const CreatedAt = styled.p`
-font-size:10px;
-color:#9e9e9e;
-opacity:0.5;
-margin:0px;
+  font-size: 10px;
+  color: #9e9e9e;
+  opacity: 0.5;
+  margin: 0px;
 `
 
 
 const ContentBox = styled.div`
-width:100%;
-box-sizing:border-box;
-height:auto;
-display:flex;
-flex-direction:column;
-justify-content:flex-start;
-aligh-items:flex-start;
-padding:0px 24px;
+  width: 100%;
+  box-sizing: border-box;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  aligh-items: flex-start;
+  padding: 0px 24px;
 `
 
 const BookTitle = styled.p`
-font-size:14px;
-line-height:20px;
-letter-spacing: -0.28px;
-color:#1168d7;
-font-weight:bold;
-margin:16px 0px 8px 0px;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: -0.28px;
+  color: #1168d7;
+  font-weight: bold;
+  margin: 16px 0px 8px 0px;
 `
 
 const Quote = styled.p`
-font-size:14px;
-font-weight: bold;
+  font-size: 14px;
+  font-weight: bold;
   line-height: 1.43;
   letter-spacing: -0.28px;
-  margin:0px 0px 16px 0px;
+  margin: 0px 0px 16px 0px;
 `
 
 const Content = styled.p`
-font-size:14px;
+  font-size: 14px;
   line-height: 1.43;
   letter-spacing: -0.28px;
-  margin:0px;
+  margin: 0px;
 `
 
 const HashTag = styled.div`
-padding:15px 0px;
-color:#1168d7;
-font-size:14px;
+  padding: 15px 0px;
+  color: #1168d7;
+  font-size: 14px;
 `
 
 const LikeBox = styled.div`
-display:flex;
-align-items:flex-end;
+  display: flex;
+  align-items: flex-end;
 `
 
 const LikeText = styled.p`
-font-size:14px;
-margin: 0px 0px 0px 8px;
-color:#1168d7;
+  font-size: 14px;
+  margin: 0px 0px 0px 8px;
+  color: #1168d7;
 `
 
 const LikeCommentBox = styled.div`
-display:flex;
-padding:10px 24px 18px 24px;
+  display: flex;
+  padding: 10px 24px 18px 24px;
 `
 
 const WriteCommentBox = styled.div`
-display:flex;
+  display: flex;
 `
 
 const CommentCount = styled.p`
-font-size:14px;
-margin: 0px 0px 0px 8px;
-color: #b5b5b5;
+  font-size: 14px;
+  margin: 0px 0px 0px 8px;
+  color: #b5b5b5;
 `
 
 export default ReviewCard;
