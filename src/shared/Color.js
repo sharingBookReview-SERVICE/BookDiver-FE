f83cacdf79158b04ec12ec26e97e9fe5f59dfff9
yechan <noy3928@naver.com>
2021-08-07 01:07:26 +0900

    [추가] 해쉬태그 제작 완료

--- src/shared/Color.js
+++ src/shared/Color.js
@@ -6,9 +6,11 @@ const Color = {
   white: "#f5f2f0",
 
   gray: "#605d5d",
-  fontgray: "#615d5d",
+  fontGray: "#615d5d",
+  borderGray: "rgba(37, 33, 33, 0.2)",
   fontblack: "#252121",
 
+  hashtag:"#e1d9d0",
   category: "#e0d9d1",
   kakao: "#f6e68c",
 };
