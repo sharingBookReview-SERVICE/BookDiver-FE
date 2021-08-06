e7c305e79f1ed464e21f586fdab5f23242fc9a0e
leehyeonju <billaa@naver.com>
2021-08-06 18:19:46 +0900

    [추가] 북 컬렉션 뷰 만드는 중

--- src/components/BookCollection.js
+++ src/components/BookCollection.js
@@ -10,7 +10,7 @@ SwiperCore.use([Navigation, Pagination])
 const BookCard = (props) =>{
     return(
         <Book>
-            <BookImage>bookCard</BookImage>
+            <BookImage></BookImage>
             <BookTitle>기획의 정석</BookTitle>
         </Book>
        
@@ -22,6 +22,7 @@ const BookCollection = (props)=>{
             <div>도비는옷이필요해</div>
             <div>주니어 UX 디자이너가 커리어를 시작 할... </div>
           <Swiper
+            style ={{margin: "0px 0px 0px 20px"}}
             spaceBetween={1}
             slidesPerView={4}
           >
@@ -37,13 +38,18 @@ const BookCollection = (props)=>{
 const Book = styled.div`
 display: flex;
 flex-direction: column;
+
 `;
 const BookImage = styled.div`
-width: 90px;
-height: 120px;
+height: 0;
 flex-grow: 0;
 border-radius: 12px;
 background: black;
+width: 90%;
+padding-bottom: 123%;
+margin: 10%;
+background-image:URL(https://i.pinimg.com/564x/2d/d4/ad/2dd4adce7f6823094e71c600ead7397e.jpg);
+background-size: cover;
 `;
 const BookTitle = styled.p`
 
