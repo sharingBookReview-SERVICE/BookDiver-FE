023e625e02e67d9e3c65a3b2ba67ebef4c1630d1
leehyeonju <billaa@naver.com>
2021-08-07 19:24:01 +0900

    [추가] 북컬렉션 메인 페이지 뷰 추가

--- src/pages/BookCollectionMain.js
+++ src/pages/BookCollectionMain.js
@@ -11,35 +11,100 @@ import Color from "../shared/Color";
 SwiperCore.use([Navigation, Pagination])
 const Collection = (props) =>{
     return (
-        <React.Fragment>
+        <Outter>
             <Image>
-
+                <CollectionTitle>
+                카페에서 가볍게 읽는 자기계발 에세이 모음
+                </CollectionTitle>
+            
             </Image>
-        </React.Fragment>
+        </Outter>
     )
 }
 const BookCollectionMain = (props) =>{
     return(
-        <React.Fragment>
-            <div>추천 컬렉션</div>
-            <Swiper
-            style ={{margin: "0px 0px 20px 20px"}}
-            spaceBetween={1}
-            slidesPerView={2}
-            >
-            <SwiperSlide>slide</SwiperSlide>
-            <SwiperSlide>slide</SwiperSlide>
-            <SwiperSlide>slide</SwiperSlide>
-            <SwiperSlide>slide</SwiperSlide>
-                </Swiper>
-        </React.Fragment>
+        <Container>
+            <CollectionIntro>테마별로, 혹은 기분으로
+                같은 무드의 도서만 모아서</CollectionIntro>
+            <Recommend>
+            <Title>추천 컬렉션</Title>
+                <Swiper
+                style ={{margin: "0px 0px 20px 20px"}}
+                spaceBetween={1}
+                slidesPerView={2}
+                >
+                <SwiperSlide><Collection/></SwiperSlide>
+                <SwiperSlide><Collection/></SwiperSlide>
+                <SwiperSlide><Collection/></SwiperSlide>
+                <SwiperSlide><Collection/></SwiperSlide>
+            </Swiper>
+            </Recommend>
+            <Recommend>
+            <Title>최신 컬렉션</Title>
+                <Swiper
+                style ={{margin: "0px 0px 20px 20px"}}
+                spaceBetween={1}
+                slidesPerView={2}
+                >
+                <SwiperSlide><Collection/></SwiperSlide>
+                <SwiperSlide><Collection/></SwiperSlide>
+                <SwiperSlide><Collection/></SwiperSlide>
+                <SwiperSlide><Collection/></SwiperSlide>
+            </Swiper>
+            </Recommend>
+            <Recommend>
+            <Title>나의 컬렉션</Title>
+                <Swiper
+                style ={{margin: "0px 0px 20px 20px"}}
+                spaceBetween={1}
+                slidesPerView={2}
+                >
+                <SwiperSlide><Collection/></SwiperSlide>
+                <SwiperSlide><Collection/></SwiperSlide>
+                <SwiperSlide><Collection/></SwiperSlide>
+                <SwiperSlide><Collection/></SwiperSlide>
+            </Swiper>
+            </Recommend>
+        </Container>
     )
 }
-
+const Container = styled.div`
+background: ${Color.mainColor};
+`;
+const CollectionIntro = styled.p`
+font-family: "Noto Serif KR", serif;
+font-size: 21px;
+width: 65%;
+font-weight: bold;
+padding: 44px 20px;
+`;
+const Outter = styled.div`
+`;
 const Image = styled.div`
 width: 160px;
 height: 160px;
 background: pink;
+border-radius: 12px;
+background-image:URL(https://i.pinimg.com/564x/1d/56/07/1d5607356a13ae7f8eb493bc2510dbf9.jpg);
+background-size: cover;
+justify-content: center;
+display: flex;
+align-items: center;
+text-align: center;
+`;
+const CollectionTitle = styled.p`
+font-family: "Noto Serif KR", serif;
+color: ${Color.white};
+margin: 0px;
+`;
+
+const Recommend = styled.div`
+padding-bottom: 20px;
+`;
+const Title = styled.div`
+margin: 20px;
+font-family: "Noto Serif KR", serif;
+font-weight : bold;
 `;
 
 export default BookCollectionMain;
