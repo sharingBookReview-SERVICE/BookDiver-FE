01b89419289f9ee742fe7686a76bbebe4298579d
yechan <noy3928@naver.com>
2021-08-20 20:23:36 +0900

    [추가] 배경화면 추가

---
+++ src/elements/Layout.js
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from 'styled-components';
+import backgroundimg from "../img/backgroundimg.png"
+import Color from "../shared/Color"
+import serviceDesc from "../img/serviceDesc.png"
+import person from "../img/mainperson.png"
+
+const Layout = (props) => {
+
+        return(
+        <>
+            <BackgroundImg url={backgroundimg}>
+                <BackgroundDesc>
+                    <ServiceDesc src={serviceDesc}/>
+                    <Person src={person}/>
+                </BackgroundDesc>
+                {props.children}
+            </BackgroundImg>
+        </>)
+}
+
+export default Layout;
+
+const BackgroundImg = styled.div`
+background-image:url(${(props) => props.url});
+background-size:cover;
+// position:fixed;
+z-index:-20;
+
+@media ${(props) => props.theme.mobile} {
+    background-image:none;
+  }
+
+@media ${(props) => props.theme.tablet} {
+    width: 100vw;
+    height:100vh;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+  }
+
+  @media ${(props) => props.theme.desktop} {
+    width: 100vw;
+    height:100vh;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+  }
+`
+
+const BackgroundDesc = styled.div`
+@media ${(props) => props.theme.mobile} {
+    display:none;
+  }
+@media ${(props) => props.theme.tablet} {
+    display:none;
+  }
+@media ${(props) => props.theme.desktop} {
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    gap:50px;
+    color:${Color.white};
+    margin-right:150px;
+  }
+`
+
+const ServiceDesc = styled.img`
+width:auto;
+height:auto;
+max-width:300px;
+max-height:300px;
+`
+
+const Person = styled.img`
+width:auto;
+height:auto;
+max-width:300px;
+max-height:300px;
+`
+
