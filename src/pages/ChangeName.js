ac4933ccc2e89305d239566d986d43b42954ebda
leehyeonju <billaa@naver.com>
2021-08-22 00:01:43 +0900

    [수정] 높이 수정

--- src/pages/ChangeName.js
+++ src/pages/ChangeName.js
@@ -94,7 +94,7 @@ const ChangeName = (props) =>{
 
 const Background = styled.div`
 width: 100%;
-height: 100%;
+height: 100vh;
 justify-content: top;
 align-items: top;
 background: ${Color.mainColor};
