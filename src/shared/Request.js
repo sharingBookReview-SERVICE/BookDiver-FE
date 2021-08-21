550195b021fc978328d9b815404ed46d33dbedbf
yechan <noy3928@naver.com>
2021-08-21 15:10:26 +0900

    [수정] 토큰이 있을때만 토큰 넣어서 보내기

--- src/shared/Request.js
+++ src/shared/Request.js
@@ -10,7 +10,9 @@ const token = localStorage.getItem('token');
 // 로그인 전이면 토큰이 없으니 못 넣어요.
 // 그럴 땐 로그인 하고 토큰을 받아왔을 때 넣어줍시다.
 // instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-instance.defaults.headers.common["Authorization"] = token ? `Bearer ${token}` : null;
+if(token){
+  instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+}
 
 // localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTEwY2Q0OTE4NWU0YTg5NTBiMTQzMzciLCJuaWNrbmFtZSI6IkxFTyIsImlhdCI6MTYyOTQ1NDY5NCwiZXhwIjoxNjI5NTQxMDk0fQ.cqs7-2M8tHlFi18glSj-DCztVB16WPIr2HeRRVnR_tA")
 
