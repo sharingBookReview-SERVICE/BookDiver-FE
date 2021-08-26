8a68a29e388bcc4d8772fa5f83659de54be4ba41
leehyeonju <billaa@naver.com>
2021-08-26 18:16:15 +0900

    [수정] booktitle split

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -35,8 +35,7 @@ const ReviewCard = (props) => {
     user,
     is_follow
   } = props;
-  // const bookTitle = book?.title.split("(")[0]
-  const bookTitle = book?.title
+  const bookTitle = book?.title.split("(")[0]
   const bookAuthor = `${book.author} 저`
 
   //permit check 
